03a70ee59c2aa55f0d5d39778cf52e1239fe5012
Pedro Sosa <pedrososa207@gmail.com>
2022-07-25 16:14:09 -0400

    initial commit with some changes

--- src/__mocks__/firebase.ts
+++ src/__mocks__/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseApp = initializeApp({
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID',
+  apiKey: 'AIzaSyDvqTwzRS74lifH--56J79ln_irSCbPc7c',
+  authDomain: 'devfest-sd-22.firebaseapp.com',
+  databaseURL: 'https://devfest-sd-22-default-rtdb.firebaseio.com',
+  projectId: 'devfest-sd-22',
+  storageBucket: 'devfest-sd-22.appspot.com',
+  messagingSenderId: '80606563354',
+  appId: '1:80606563354:web:51172a0114bb2b111115a3',
+  measurementId: 'G-96HT1GMXQ9',
 });
 export const db = getFirestore(firebaseApp);
--- src/firebase.ts
+++ src/firebase.ts
@@ -19,7 +19,17 @@ declare global {
   }
 }
 
-const firebaseConfig = window.firebaseConfig;
+// const firebaseConfig = window.firebaseConfig;
+const firebaseConfig = {
+  apiKey: 'AIzaSyDvqTwzRS74lifH--56J79ln_irSCbPc7c',
+  authDomain: 'devfest-sd-22.firebaseapp.com',
+  databaseURL: 'https://devfest-sd-22-default-rtdb.firebaseio.com',
+  projectId: 'devfest-sd-22',
+  storageBucket: 'devfest-sd-22.appspot.com',
+  messagingSenderId: '80606563354',
+  appId: '1:80606563354:web:51172a0114bb2b111115a3',
+  measurementId: 'G-96HT1GMXQ9',
+};
 
 if (!firebaseConfig) {
   throw new Error('window.firebaseConfig is not defined');
